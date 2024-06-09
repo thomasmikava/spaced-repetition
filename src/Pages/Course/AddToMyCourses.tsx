b89256f81a2dd29beb09b8701529660c44718064
Mikava Temuri <koddddddi@gmail.com>
2024-06-09 15:50:52 +0400

    add loading mode for start course button

--- src/Pages/Course/AddToMyCourses.tsx
+++ src/Pages/Course/AddToMyCourses.tsx
@@ -3,14 +3,15 @@ import cssModule from '../../App.module.css';
 import { useAddToMyCourses } from '../../api/controllers/courses/courses.query';
 
 export const AddToMyCoursesButton: FC<{ courseId: number }> = ({ courseId }) => {
-  const { mutate } = useAddToMyCourses();
+  const { mutate, isPending } = useAddToMyCourses();
   const handleAddToMyCourses = () => {
+    if (isPending) return;
     mutate({ courseId });
   };
 
   return (
     <div className={cssModule.buttonsContainer}>
-      <button onClick={handleAddToMyCourses}>Start this course</button>
+      <button onClick={handleAddToMyCourses}>Start this course {isPending ? '...' : ''}</button>
     </div>
   );
 };
