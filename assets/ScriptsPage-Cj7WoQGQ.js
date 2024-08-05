f48bdcaed40e2c1471458ea1c17ab3ff06b04a1a
Mikava Temuri <koddddddi@gmail.com>
2024-08-06 00:12:21 +0400

    Updates

--- assets/ScriptsPage-Cj7WoQGQ.js
+++ assets/ScriptsPage-Cj7WoQGQ.js
@@ -1,4 +1,4 @@
-import{u as c,a as m,j as n,S as p,B as a,C as e}from"./index-BJBmlsGt.js";const f=`function cpVerbConvertMoodName(moodName) {\r
+import{u as c,a as m,j as n,S as p,B as a,C as e}from"./index-DkrvrfTk.js";const f=`function cpVerbConvertMoodName(moodName) {\r
   if (!moodName) throw new Error('undefined mood');\r
   switch (moodName) {\r
     case 'Indicatif':\r
--- assets/TestingThings-CySXAFCO.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BJBmlsGt.js";const t=()=>s.jsx("div",{className:"body",children:"Test"});export{t as default};
