891c5a760b07858956dd3f12d94f29c9cab764a8
Mikava Temuri <koddddddi@gmail.com>
2024-09-16 20:17:01 +0400

    Updates

--- assets/ScriptsPage-ChGuVjSm.js
+++ assets/ScriptsPage-ChGuVjSm.js
@@ -1,4 +1,4 @@
-import{u as c,a as m,j as n,S as p,B as a,C as e}from"./index-CuiRSyaK.js";const f=`function cpVerbConvertMoodName(moodName) {\r
+import{u as c,a as m,j as n,S as p,B as a,C as e}from"./index-Db4qscid.js";const f=`function cpVerbConvertMoodName(moodName) {\r
   if (!moodName) throw new Error('undefined mood');\r
   switch (moodName) {\r
     case 'Indicatif':\r
