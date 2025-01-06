c5ae2a460457785a4d0364be7a406430b79d9389
Mikava Temuri <koddddddi@gmail.com>
2025-01-07 01:11:48 +0400

    Updates

--- assets/ScriptsPage-BCyZND3b.js
+++ assets/ScriptsPage-BCyZND3b.js
@@ -1,4 +1,4 @@
-import{u as c,a as m,j as n,S as p,B as a,C as e}from"./index-BjAZJE64.js";const f=`function cpVerbConvertMoodName(moodName) {\r
+import{u as c,a as m,j as n,S as p,B as a,C as e}from"./index-C0TezJm-.js";const f=`function cpVerbConvertMoodName(moodName) {\r
   if (!moodName) throw new Error('undefined mood');\r
   switch (moodName) {\r
     case 'Indicatif':\r
