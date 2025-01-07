1a1eae5b1c27bbf13f93833e8c5940d0096031db
Mikava Temuri <koddddddi@gmail.com>
2025-01-07 16:20:38 +0400

    Updates

--- assets/TestingThings-C5rSYERJ.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Dwy1i1xs.js";const t=()=>s.jsx("div",{className:"body",children:"Test"});export{t as default};
---
+++ assets/TestingThings-DEsjdmMD.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CawRjxkp.js";const t=()=>s.jsx("div",{className:"body",children:"Test"});export{t as default};
